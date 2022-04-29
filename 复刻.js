ef773871c1f54dc7ab47096e61c6fb6b0c9c78db
itawenya <93374574+itawenya@users.noreply.github.com>
2022-04-29 14:49:14 +0800

    prove

--- 复刻.js
+++ 复刻.js
@@ -1,29 +1,68 @@
-/*复制邮箱链接 */
-function copyToClip(content, message) {
+window.onload = function () {
+  /*复制邮箱链接 */
+  var btn = document.getElementById("a2");
+  var content = "newthread_geek@outlook.com";
+  btn.onclick = function copyToClip() {
     var aux = document.createElement("input");
     aux.setAttribute("value", content);
     document.body.appendChild(aux);
     aux.select();
     document.execCommand("copy");
     document.body.removeChild(aux);
-    if (message == null) {
       alert("Geek的邮箱已经复制到剪贴板了！♡(*´∀｀*)人(*´∀｀*)♡");
-    } else {
-      alert(message);
-    }
   }
 
+  /*问答 */
+  var ul = document.getElementById("QA-ul");
+  var ulChildren = ul.children;
+  for (var i = 0; i < ulChildren.length; i++) {
+    ulChildren[i].onclick = function (event) {
+      event = event || window.event;
+      if(event.target.className == "QA-a"){
+        if (this.children[1].style.display == "none") {
+          this.children[1].style.display = "block";
+        } else {
+          this.children[1].style.display = "none";
+        }
+      };
+      };
+  }
 
-/*问答 */
-var LU = document.querySelector("#lu");
-var lu = LU.children;
-var flag = 0;
-for (var i = 0; i < lu.length; i++) {
-  lu[i].onclick = function () {
-    if (this.children[1].style.display == "none") {
-      this.children[1].style.display = "block";
-    } else {
-      this.children[1].style.display = "none";
-    }
+  /*丝滑跳转*/
+  var about = document.getElementById("about");
+  var aboutusplace = document.getElementById("aboutusplace");
+  about.onclick = function(){
+    aboutusplace.scrollIntoView({behavior:"smooth"}) ;
   };
-}
+
+  var introduction = document.getElementById("introduction");
+  var pass = document.getElementById("pass");
+  introduction.onclick = function(event){
+    event = event || window.event;
+    if(event.target.className == "intro_a"){
+      pass.scrollIntoView({behavior:"smooth"}) ;
+    };
+    
+  };
+
+  var qa = document.getElementById("qa");
+  var QAplace = document.getElementById("QAplace");
+  qa.onclick = function(){
+    QAplace.scrollIntoView({behavior:"smooth"}) ;
+  };
+
+  var contact = document.getElementById("contact");
+  var contactplace = document.getElementById("contactplace");
+  contact.onclick = function(){
+    contactplace.scrollIntoView({behavior:"smooth"}) ;
+  };
+
+  var pointer2 = document.getElementById("pointer2");
+  var top = document.getElementById("top");
+  pointer2.onclick = function(){
+    top.scrollIntoView({behavior:"smooth"}) ;
+  };
+};
+
+
+
